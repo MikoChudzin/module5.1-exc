af4dded31fcf70635f22403c6b0a537d8587e5a9
Miko Chudzin <miko.chudzinski@gmail.com>
2024-07-08 19:54:28 +0200

    Add basic docstring

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s', filen
 
 # Exercise - Address Book
 class BusinessCard:
+    """
+    Klasa przechowująca dane z wizytówek
+    Argumenty:
+    imię, nazwisko, nazwa firmy, stanowisko, adres e-mail
+    """
     def __init__(self):
        pass
 
